ff694cb953c82f50df5a7fdff9509f1dafed44c3
irinaromanova <romanova_98@mail.ru>
2024-06-19 22:27:41 +0300

    добавила автогенерацию

--- src/main/kotlin/ru/wedding/expenses/model/Expense.kt
+++ src/main/kotlin/ru/wedding/expenses/model/Expense.kt
@@ -2,10 +2,13 @@ package ru.wedding.expenses.model
 
 import jakarta.persistence.Id
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 
 @Entity
 data class Expenses(
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Int,
     var cost: Double,
     var name: String,
